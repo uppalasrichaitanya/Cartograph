72291fd26d96aca70d2c132a3a7a251b65de5de6
uppalasrichaitanya <uppalasrichaitanya2007@gmail.com>
2026-08-05 16:25:10 +0530

    fix: explicit output file tracing for tree-sitter wasms

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["adm-zip", "typescript", "elkjs", "web-tree-sitter", "tree-sitter-wasms"],
+  outputFileTracingIncludes: {
+    "/api/**/*": [
+      "./node_modules/tree-sitter-wasms/out/**/*.wasm",
+      "./node_modules/tree-sitter-wasms/package.json",
+      "./node_modules/web-tree-sitter/**/*.wasm"
+    ],
+  },
 };
 
 export default nextConfig;
